4c21da296c7fe76443442accbd0575b665b4fa33
Valentin Malindi <valentin.malindi@iut-dijon.u-bourgogne.fr>
2025-05-13 16:24:59 +0200

    Implementation de la fonction SetSetting de la classe SettingDAO

--- taskstep/model/SettingDAO.php
+++ taskstep/model/SettingDAO.php
@@ -3,7 +3,7 @@ require_once("Database.php");
 class SettingDAO extends Database
 {
     public function setSetting(string $type,string $value){
-
+        $this->execute("UPDATE settings SET value= :value WHERE setting=:setting",[':value' => $value,':setting' => $type]);
     }
 
     public function getSetting(string $type) : string{
