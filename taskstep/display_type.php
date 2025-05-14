5bbf861c8782e4b49f28ceaf596d134441e31cc1
Valentin Malindi <valentin.malindi@iut-dijon.u-bourgogne.fr>
2025-05-14 12:02:58 +0200

    Simplification du code de la page Display_type

--- taskstep/display_type.php
+++ taskstep/display_type.php
@@ -1,6 +1,5 @@
 <?php
 include("includes/header.php");
-require_once("model/SectionDAO.php");
 require_once("model/ContextDAO.php");
 require_once("model/ProjectDAO.php");
 
@@ -8,41 +7,32 @@ $sortby = (isset($_GET["sort"])) ? $_GET["sort"] : '';
 
 $type = (isset($_GET['type'])) ? $_GET['type'] : '';
 
+?>
 
-echo "<div id='editlist'>\n<p>".$l_dbp_l1[$type]."</p>";
-//display the add project/context link
-echo "<a href='edit_types.php?type=$type&amp;cmd=add' class='listlinkssmart'><img src='images/add.png' alt='' /> ".$l_dbp_add[$type]."</a>";
+<div id='editlist'>
+	<p><?= $l_dbp_l1[$type] ?></p>
+	<a href='edit_types.php?type=$type&amp;cmd=add' class='listlinkssmart'><img src='images/add.png' alt='' /> <?= $l_dbp_add[$type] ?></a>
 
-if ($type == "context"){
-	$contextdb = new ContextDAO();
-	$contexts = $contextdb->getAll();
-	
-	
-	foreach($contexts as $s){
-		//grab the title and the ID of the project/context
-		$title=$s->getTitle();
-		$id=$s->getId();
+	<?php
+		if ($type == "context"){
+			$contextdb = new ContextDAO();
+			$res = $contextdb->getAll();
+		}
 
-		//make the title a link
-		echo "<a href='display.php?display=$type&amp;tid=$id&amp;sort=date' class='listlinkssmart'><img src='images/$type.png' alt='' /> $title</a>";
-	}
-}
+		if ($type == "project"){
+			$contextdb = new ProjectDAO();
+			$res = $contextdb->getAll();
+		}
+	?>
 
-if ($type == "project"){
-	$contextdb = new ProjectDAO();
-	$contexts = $contextdb->getAll();
-	
-	foreach($contexts as $s){
-		//grab the title and the ID of the project/context
-		$title=$s->getTitle();
-		$id=$s->getId();
-
-		//make the title a link
-		echo "<a href='display.php?display=$type&amp;tid=$id&amp;sort=date' class='listlinkssmart'><img src='images/$type.png' alt='' /> $title</a>";
-	}
-}
-
-echo "\n<a href='edit_types.php?type=$type' class='listlinkssmart'><img src='images/{$type}_edit.png' alt='' /> ".$l_dbp_edit[$type]."</a>\n</div>";
+			<?php foreach($res as $s) : ?>
+				<?php
+					$title=$s->getTitle();
+					$id=$s->getId();
+				?>
+				<a href="display.php?display=<?= $type?>&tid=<?= $id ?>&sort=date" class='listlinkssmart'><img src='images/<?= $type ?>.png' alt='' /> <?= $title ?></a>
+			<?php endforeach; ?>
+<a href="edit_types.php?type=<?= $type?>" class='listlinkssmart'><img src='images/<?= $type ?>_edit.png' alt='' /> <?= $l_dbp_edit[$type] ?></a></div>
 
 include('includes/footer.php');
 ?>
