4a22f5fe520ae811977b5d0b61faf1161590c782
oe543944 <134264751+oe543944@users.noreply.github.com>
2025-05-15 21:05:56 +0200

    Update de l'installation
    L'installation passe maintenant par un fichier insertUser.php avant l'installation du reste de l'application car le mots de passe doit être hasher avant d'être insérer ce qui n'est pas possible dans tasktep.sql

---
+++ taskstep/Services/insertUser.php
@@ -0,0 +1,33 @@
+<?php
+// Configuration de la base de données
+$host = 'localhost'; // ou l'adresse de votre serveur de base de données
+$db = 'taskstepnew';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+
+// DSN (Data Source Name)
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+try {
+    // Établir la connexion
+    $pdo = new PDO($dsn, $user, $pass, $options);
+
+    // Préparer la requête d'insertion
+    $stmt = $pdo->prepare("INSERT INTO users (email, password) VALUES (:email, :password)");
+
+    // Définir les valeurs à insérer
+    $email = 'tata@gmail.com';
+    $password = password_hash('toto', PASSWORD_DEFAULT); // Hachage du mot de passe
+
+    // Lier les paramètres et exécuter la requête
+    $stmt->execute(['email' => $email, 'password' => $password]);
+
+    echo "Données insérées avec succès.";
+} catch (\PDOException $e) {
+    // Gérer les erreurs
+    echo "Erreur lors de l'insertion : " . $e->getMessage();
+}
+?>
