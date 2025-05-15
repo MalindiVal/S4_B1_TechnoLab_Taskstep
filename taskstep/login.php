b5b2d32da7a0537a5da08c652cfb05dec925e279
oe543944 <134264751+oe543944@users.noreply.github.com>
2025-05-15 21:03:52 +0200

    Update de login.php
    La vue maintenant possède nettement moins de responsabilité il manquerai un routeur pour la libérer de l'appelle des controller .

--- taskstep/login.php
+++ taskstep/login.php
@@ -3,42 +3,12 @@ session_start();
 
 include("config.php");
 include("includes/functions.php");
-require_once("model/SettingDAO.php");
-$settingdb = new SettingDAO();
-$mysqli = connect();
+include("Controller/LoginController.php");
 
-$failed = false;
+//Appelle de la méthode permettant la connexiuon
+$userController = new LoginController();
+$user = $userController->connexion();
 
-if (isset($_POST["submit"]))
-{
-	$hashpassword = $settingdb->getSetting('password');
-	$salt = $settingdb->getSetting('salt');
-	$setting['password'] = $hashpassword;	//Build a multi-dimensional array containing the returned rows
-	$setting['salt'] = $salt;
-	
-	$given = $_POST["password"];
-	/*if (password_verify($_POST["password"],$hashpassword))
-	{*/
-		$_SESSION["loggedin"] = true;
-		$host  = $_SERVER['HTTP_HOST'];
-		$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		$extra = 'index.php';
-		session_write_close();
-		header("Location: http://$host$uri/$extra");
-		exit;
-	/*}
-	else
-	{
-		$failed = true;
-		$_SESSION["loggedin"] = false;
-	}*/
-}
-else if (isset($_GET["action"])) $_SESSION['loggedin'] = false;	//If "action" is set, log out
-
-//if($_SESSION['loggedin'] == true)
-//{
-//  echo "You're already logged in! Either <a href='logout.php'>logout</a> or continue to the <a href='index.php'>main page.</a>";
-//}
 
 header("Cache-control: private");
 include("lang/".$language.".php");
@@ -49,9 +19,9 @@ include("lang/".$language.".php");
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>TaskStep - Login</title>
 <?php 
-	$db = new SettingDAO();
-	$value = $db->getSetting('style');
-	echo "<link rel='stylesheet' type='text/css' href='styles/".$value."' media='screen' />";
+	//$db = new SettingDAO();
+	//$value = $db->getSetting('style');
+	//echo "<link rel='stylesheet' type='text/css' href='styles/".$value."' media='screen' />";
 ?>
 </head>
 
@@ -60,17 +30,26 @@ include("lang/".$language.".php");
 <!--Open container-->
 <div id="container">
 <?php
-$sessionssetting = $settingdb->getSetting('sessions');
+//$sessionssetting = $settingdb->getSetting('sessions');
+$sessionssetting = 1 ;
 ?>
 <div id="loginbox">
 <h1><img src="images/icon.png" alt="" /> TaskStep</h1>
 <?php if($sessionssetting == '1'){ ?>
 <p><img src="images/shield.png" alt="" />&nbsp;<?php echo $l_login_l1; ?></p>
 <form action="login.php" method="post">
-<p>
-<input type="password" name="password" />&nbsp;
-<input type="text" style="display: none;" />	<!--IE workaround: pressing "enter" will submit the form-->
-<input type="submit" name="submit" value="<?php echo $l_login_button; ?>" /></p>
+	<div>
+		<label for="MailConnexion">Email</label>
+		<input type="text" name="identifiant">
+	</div>
+	<div>
+		<label for="PwdConnexion">Passord</label>
+		<input type="password" name="password" />&nbsp;
+	</div>
+	<div>
+		<input type="text" style="display: none;" />	<!--IE workaround: pressing "enter" will submit the form-->
+		<input type="submit" name="submit" value="<?php echo $l_login_button; ?>" />
+	</div>
 </form> <?php }
 else{ ?>
   <p><img src="images/shield_error.png" alt="" />&nbsp;<?php echo $l_login_l5; ?></p>
@@ -83,7 +62,11 @@ else{ ?>
 //Uncomment the next line for session debugging
 //echo $_SESSION["loggedin"];
 
-if ($failed) echo "<p><img src='images/cross.png' alt='' /> ".$l_login_l4."</p>";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($user == null) {
+        echo "<p><img src='images/cross.png' alt='' /> ".$l_login_l4."</p>";
+    }
+}
 ?>
 
 <span class="securityinfo">TaskStep login system version 1.0</span>
