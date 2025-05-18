7264de84b37586facdd59a2e38c3e2fda36a32df
oe543944 <134264751+oe543944@users.noreply.github.com>
2025-05-18 22:28:39 +0200

    Ajout du Model pour récuprérer les paramètre

--- taskstep/model/SettingDAO.php
+++ taskstep/model/SettingDAO.php
@@ -3,27 +3,29 @@ require_once("Database.php");
 require_once("Setting.php");
 class SettingDAO extends Database
 {
-
     /**
      * Summary of getAll
      * @return array
      */
-    public function getAll(): Setting {
-        $tab = array();
+    public function getSettingsByUser($idUser): Setting {
+        try{
+            $sql = "SELECT id, user_id, tips, stylesheet, `session` FROM settings WHERE user_id = ?";
+            $res = $this->queryOne($sql, [$idUser]);
 
-        $res = $this->queryOne("SELECT tips, stylesheet, session FROM settings WHERE user_id = :uid", [
-            ':uid' => intval($_SESSION["user_id"])
-        ]);
-
-        $setting = new Setting();
-        $setting->hydrate($res);
+            $setting = new Setting();
+            $setting->hydrate($res);
+        }
+        catch(Exception $e){
+            throw new Exception("Erreur lors de l'exécution de la requête: " . $e->getMessage());
+        }
 
         return $setting;
+    
     }
     
 
     public function UpdateSetting(Setting $settings): void {
-        $sql = "UPDATE settings SET tips = :tips, stylesheet = :stylesheet, session = 1 WHERE user_id = :uid";
+        $sql = "UPDATE settings SET tips = :tips, stylesheet = :stylesheet, `session` = 1 WHERE user_id = :uid";
         $this->execute($sql, [
             ':tips' => $settings->getTips(),
             ':stylesheet' => $settings->getStylesheet(),
